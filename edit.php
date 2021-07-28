b3f71351e5ff2cecae1572d3e71d3f5e91969eb8
Esamai Emmanuel <Esamaiemmanuel@users.noreply.github.com>
2021-07-28 13:24:07 +0300

    Update edit.php

--- edit.php
+++ edit.php
@@ -59,7 +59,8 @@ if(!isset($_GET['id'])){
   </div> 
 
   
-  <button type="submit" name= "submit" class="btn btn-success btn-block">Save Changes</button>
+  <a  href="viewrecords.php" class="btn btn-default">Back To List</a>
+  <button type="submit" name= "submit" class="btn btn-success ">Save Changes</button>
 </form>    
 <?php } ?>
 <br>
